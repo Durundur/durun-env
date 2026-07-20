62b8c3b6203f3b70fa687d0ebb68f85a66ce24a8
Durundur <0bogusz0@gmail.com>
2026-07-20 23:24:47 +0200

    feat: deploy runner

--- deploy-runner/entrypoint.sh
+++ deploy-runner/entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+GITHUB_TOKEN=$(curl -sL \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${DEPLOY_RUNNER_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2026-03-10" \
+  https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token \
+  | jq -r '.token')
+
 ./config.sh --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
     --token ${GITHUB_TOKEN} \
     --name "deploy-runner" \
